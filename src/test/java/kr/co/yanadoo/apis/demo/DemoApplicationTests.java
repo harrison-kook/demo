d079f1230e6553c8108a6af914a8d7ad947360a7
harrison-kook <121649174+harrison-kook@users.noreply.github.com>
2022-12-30 11:56:41 +0900

    Initial commit

---
+++ src/test/java/kr/co/yanadoo/apis/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.yanadoo.apis.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
